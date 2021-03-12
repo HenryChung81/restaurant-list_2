1a7bbf04930f3171a0b9d7b06fc5a140b900f080
Henry Chung <chungjenhao81@gmail.com>
2021-03-12 09:08:39 +0800

    feat:add express-handlebars

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
   res.send(`<h1>hello world!</h1>`)
 })
