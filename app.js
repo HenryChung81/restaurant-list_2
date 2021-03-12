5b911f0bc2fa2caf0a5c2a368ea132baf2bb0c41
Henry Chung <chungjenhao81@gmail.com>
2021-03-12 10:55:55 +0800

    feat:restaurant show page

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results })
+})
+app.get('/restaurant/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () => {
